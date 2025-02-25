ad695f29e749bcd10409ad80006cd141ca072e59
Fatih Çakmakçı <31661534+fatihc07@users.noreply.github.com>
2025-02-25 11:15:43 +0300

    Add files via upload

---
+++ ders 1-2/index.js
@@ -0,0 +1,172 @@
+//  let,const,var   -----
+
+/*
+Char string number, float, Boolean
+
+'0', null ,0
+
+
+
+22333223
+'aasdfsd'
+a
+
+var a = 10;
+const b = 20;
+let c = 30;
+
+if (a > b) {
+    console.log('a is greater than b');
+    
+
+} else if (a == b) {
+    console.log("a eşittir b'ye");
+}
+
+else {
+    console.log('a is less than b');
+}
+
+
+ 
+
+
+        
+
+
+
+  switch (a) {
+    case a>b:
+      console.log('a is 1');
+      break;
+     
+    case a==b:
+      console.log('a is 2');
+      break;
+    
+    default:
+      console.log('default');
+        break;
+    case a<b:
+      console.log('a kucuktur');
+      break;
+  }
+
+// döngüler
+
+// for, while, do while, foreach
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    //i++
+}
+
+// a=a+1  = a++
+
+var d = 0;
+while (d < 10) {
+    console.log(a);
+    d++;
+}
+
+var e = 0;
+do {
+    console.log(e);
+    e++;
+} while (e < 10);     
+
+// array
+
+
+var dizi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,'aa'];
+dizi.push(11);
+dizi.pop();
+
+
+for (let i = 0; i < dizi.length; i++) {
+
+    console.log( "Dizimin" +   dizi[i]+1 + "elemanı"+ dizi[i]+ "elemanıdır");
+}
+console.log(dizi);
+
+
+
+// function
+
+function toplam(a, b) {
+    return a + b;
+    
+}
+
+function topla(a, b) {
+  
+console.log(a + b);
+
+
+}
+toplam(5,6);
+console.log(toplam);
+
+topla(5,6);
+
+
+function kullaniciismi() {
+
+  return 'kullanici adi';
+
+
+}
+
+
+console.log(kullaniciisma());
+
+
+1 den 5 e  kadar dizi oluşturup, 3. elemanı ekrana yazıdran fonksiyonu yazınız.
+
+dizi2=1,2,3,4,5;
+function dizi(dizif)
+
+console.log("dizinin 3. elamanı "+ dizif 2);
+
+ dizi(dizi2);
+
+
+function dizi3()
+Array=1,2,3;
+for(let i =0; i 4)
+  console.log("Dizinin: ", Array);
+
+dizi3();
+*/
+
+var yazi1="Selamlar"
+var yazi2="Javascript"
+const sayi=5
+let onda=10.5
+var sayi5="5"
+
+console.log(yazi1.toUpperCase)
+console.log(yazi1.replaceAll('a','e'))
+sayi.toString
+sayi5.Number
+
+
+"35"
+string>Number
+number>string
+float>number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
