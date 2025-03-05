0cd8754db6c6d3d21e9cc5659eb22bdb527b107d
Fatih Çakmakçı <31661534+fatihc07@users.noreply.github.com>
2025-03-05 16:42:23 +0300

    Add files via upload

---
+++ ders5/ders5.js
@@ -0,0 +1,20 @@
+// body gibi diğer etiketlere etki etme
+// document.body.style.backgroundColor = "red";
+// ikinci yol
+document.getElementsByTagName("body").style.backgroundColor="yellow"
+
+// innerHTML, innerText, value: form elementlerinin içindekileri
+
+var liste= ['Arda','Tuğba','Esra']
+
+
+// arrayde arama yapar
+if (liste.includes("Tuğba")){
+    // işlem yapsın
+
+}
+
+
+
+
+
