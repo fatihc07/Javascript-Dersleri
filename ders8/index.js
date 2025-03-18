dd326445e02152f9bb45c4ce3f65d3689c1b2fad
Fatih Çakmakçı <31661534+fatihc07@users.noreply.github.com>
2025-03-18 13:25:11 +0300

    Add files via upload

---
+++ ders8/index.js
@@ -0,0 +1,92 @@
+// fonksiyonlar
+// return kullanımı farkları
+
+// değer döndüren vs döndürmeyen
+// return sadece functionlarda kullanılır.
+// Math.random fonsiyonu 0 la 9 arası sayı üretir
+// Math. floor fonsiyonu ondalıklı sayıyı tam sayı yapar
+
+
+let ad= prompt("Adınızı girin: ")
+let degis=donen(ad)
+ekle()
+
+function donen (isim){
+
+    return prompt("Döndü!"+ isim)
+
+}
+
+function ekle () {
+
+    console.log (degis)
+    
+
+}
+
+
+
+let sayi1= prompt("Sayı: ")
+let sayi2= prompt("Sayı2: ")
+islem(sayi1,sayi2)
+
+function islem(sayi1,sayi2){
+
+if(sayi1<sayi2) {
+
+    return  alert ("sayi 1 büyüktür")
+    
+
+}
+
+else if(sayi1>sayi2) {
+
+    return  alert ("sayi 2 büyüktür")
+    
+
+}
+
+
+else {
+
+    console.log("eşittir")
+}
+
+}
+
+
+let randomsayi=uret() // while döngüsünde olamaz, yoksa her defasında yeniden sayı üretilir.
+console.log("Üretilen sayı:" +randomsayi)
+let sayi =5  // global değişken
+do {
+    let sayi= prompt("Sayı girin: ")       // sayi değişkeni burda değişir. artık 5 değil
+    if(randomsayi<sayi){
+        alert("büyük  bir sayı girdiniz")
+    
+    }
+
+     else if(randomsayi>sayi){
+        alert("küçük  bir sayı girdiniz")
+    
+    }
+
+
+}while (randomsayi!=sayi) // burda sayi tekrar 5 değerine döner
+   alert("Bildiniz")
+
+
+
+
+function uret(){
+
+    return Math.floor(Math.random()*10)
+
+
+
+}
+
+
+
+
+
+
