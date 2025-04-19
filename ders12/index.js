a2e7d832705ce0bd7e0f6ba5b1955a3a8bca81ec
Fatih Çakmakçı <31661534+fatihc07@users.noreply.github.com>
2025-04-19 21:34:05 +0300

    Add files via upload

---
+++ ders12/index.js
@@ -0,0 +1,46 @@
+document.getElementById('deneme').addEventListener('click',function(){
+
+
+
+})
+
+// Math.round()
+// Math.rand()
+// 18,3=18
+// 18,8=19
+
+function kontrol(){
+
+        Array =[0,1,2]
+        foreach Array in a{
+            Array[a]
+
+        }
+
+        for(i;i>5;i++)
+
+
+
+            
+
+
+
+
+}
+
+
+
+Mouse’un üzerine gelmek (mouseover) ya da çıkmak (mouseout)
+document.getElementById('yazi').addEventListener('keyup', function()  {
+
+    // p=girilen_yazi
+    // input=yazi
+
+    document.getElementById('girilen_yazi').innerText=document.getElementById('yazi').value
+
+
+
+})
+
+// mouseOver fonksiyonunu kullanarak fare divin üstüne gelince üstünde yazsın
+//  mouseOut fare divde değilken üstünde değildir.
